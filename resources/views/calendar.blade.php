93a18a969d531247cfe01feada6ee373c8d17b21
EC2 Default User <ec2-user@ip-172-26-6-221.ap-northeast-1.compute.internal>
2021-06-16 11:27:50 +0000

    make calendar template

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Twical</title>
+    </head>
+    <body>
+        <h1>Twical</h1>
+    </body>
+</html>
