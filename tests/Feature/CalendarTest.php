84206a149277004de5f4b01be99d6b6424db1be0
rotelstift <rotelstift@gmail.com>
2021-06-18 13:42:50 +0000

    replace today as edge case

--- tests/Feature/CalendarTest.php
+++ tests/Feature/CalendarTest.php
@@ -10,7 +10,7 @@ class CalendarTest extends TestCase
 {
     protected function setUp(): void
     {
-        $target_date = Carbon::create(2021, 5, 18, 12, 00, 0);
+        $target_date = Carbon::create(2020, 1, 31, 12, 00, 0);
         Carbon::setTestNow($target_date);
         parent::setUp();
     }
@@ -31,12 +31,12 @@ class CalendarTest extends TestCase
         $responseData = $response->getOriginalContent()->getData();
 
         $response->assertStatus(200);
-        $this->assertEquals("2021", $responseData['year']);
-        $this->assertEquals("5", $responseData['month']);
-        $this->assertEquals("/2022/05", $responseData['nextYearPath']);
-        $this->assertEquals("/2020/05", $responseData['prevYearPath']);
-        $this->assertEquals("/2021/06", $responseData['nextMonthPath']);
-        $this->assertEquals("/2021/04", $responseData['prevMonthPath']);
+        $this->assertEquals("2020", $responseData['year']);
+        $this->assertEquals("1", $responseData['month']);
+        $this->assertEquals("/2021/01", $responseData['nextYearPath']);
+        $this->assertEquals("/2019/01", $responseData['prevYearPath']);
+        $this->assertEquals("/2020/02", $responseData['nextMonthPath']);
+        $this->assertEquals("/2019/12", $responseData['prevMonthPath']);
     }
     
     /**
