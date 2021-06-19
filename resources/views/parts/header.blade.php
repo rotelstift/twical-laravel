7276f4a2c3c1466b748bee99d5bc35ac76f2695e
rotelstift <rotelstift@gmail.com>
2021-06-19 08:19:09 +0000

    make collapse menu

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,5 +1,6 @@
-<header>
-    <h1 class="bg-primary display-1 p-2">
+<header class='navbar bg-primary mb-3'>
+    <h1 class="display-1 p-2">
         <a href="/" class="text-decoration-none link-light">Twical</a>
     </h1>
+    @include('parts.menu')
 </header>
