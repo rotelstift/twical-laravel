8d34455948334c2d4ebc9cdb71ccc00695abf6f3
rotelstift <rotelstift@gmail.com>
2022-05-16 14:41:45 +0000

    ログインコントローラーの雛形を作成

---
+++ app/Http/Controllers/TwitterLoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Exception;
+
+class TwitterLoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
