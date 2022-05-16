aa0cfd597281a07d04e01c1ce11ac653ce7a693c
rotelstift <rotelstift@gmail.com>
2022-05-16 23:44:19 +0000

    ログインボタンの色を変更

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 @extends('parts.layout')
 @section('content')
   <div class="flex items-center justify-end mt-4">
-    <a class="btn" href="{{ route('login.twitter') }}">
+    <a class="btn btn-primary" href="{{ route('login.twitter') }}">
       Login with Twitter
     </a>
   </div>
